a7ad25abcae00141c267d700e761f11e8c01de90
Matheus Ferreira <ferreiromagrelo@gmail.com>
2022-10-10 18:23:21 -0300

    feat: middleware jwt ensureAuth

---
+++ server/src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,35 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+interface ITokenPayload {
+  id: string;
+  iat: number;
+  exp: number;
+}
+
+export default function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+): void | Response {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader)
+    return response.status(401).json({ error: "No token provided" });
+
+  const parts = authHeader.split(" ");
+  if (parts.length !== 2)
+    return response.status(401).json({ error: "Invalid token" });
+
+  const [scheme, token] = parts;
+  if (!/^Bearer$/i.test(scheme))
+    return response.status(401).json({ error: "Token malformatted" });
+
+  if (!process.env.JWT_SECRET) {
+    return response.status(401).json({ error: "Invalid JWT enviremenets" });
+  }
+  const { id } = verify(token, process.env.JWT_SECRET) as ITokenPayload;
+  if (!id) throw new Error("Invalid token");
+  request.user = { id: id };
+  return next();
+}
